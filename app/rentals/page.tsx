fa0bb344648a58273a8e738c94514097e76aa716
conta09 <mucyoconta8@gmail.com>
2026-01-21 14:49:02 +0200

    added routes

---
+++ app/rentals/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PropertiesGrid from '../components/PropertiesGrid'
+function rentals() {
+  return (
+    <div>
+        <PropertiesGrid />
+    </div>
+  )
+}
+
+export default rentals
---
+++ app/sell/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function seller() {
+  return (
+    <div>
+      seller
+    </div>
+  )
+}
+
+export default seller
